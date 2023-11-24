8505481db147d170450f8296f5859d9d52e84e5f
echeadle <echeadle@gmail.com>
2023-11-24 11:19:12 -0700

    First try with copilot rewriting and improving translate.

--- translate.py
+++ translate.py
@@ -1,46 +1,41 @@
 #!/usr/bin/env python3
 """
 Author : echeadle <echeadle@localhost>
-Date   : 2023-09-16
+Author : Copilot
+Date   : 2023-11-24
 Purpose: Take a string and translate it to a list of numbers
 """
 
 import argparse
 import re
 
-# --------------------------------------------------
 def get_args():
-    """Get command-line arguments"""
-
+    """Parse command-line arguments"""
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Take a string and translate it to a list of numbers',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
+    parser.add_argument('string',
                         metavar='str',
-                        help='A positional argument')
-
+                        help='Input string')
 
     return parser.parse_args()
 
 
-# --------------------------------------------------
-def main():
-    """Translate string to list of numbers"""
+def extract_numbers(string_arg):
+    nums = re.findall(r'\d+', string_arg)
     new_list = []
-    args = get_args()
-    pos_arg = args.positional
-
-    print(f'positional = "{pos_arg}"')
-    #pos_arg = pos_arg.replace('-', '')
-    nums = re.findall(r'\d+', pos_arg)
-    print(f'nums: {nums}')
     for n in nums:
         new_list.append(int(n))
+    return new_list
 
-    print(new_list)
-        
 
-# --------------------------------------------------
+def main():
+    args = get_args()
+    string = args.string
+    numbers = extract_numbers(string)
+    print(numbers)
+
+
 if __name__ == '__main__':
     main()
